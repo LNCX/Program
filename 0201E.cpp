ed0afbe35824ae7496704a828aa3dd0e4dac222e
LNCX <1367595063@qq.com>
2019-02-01 10:44:44 +0800

    0201

---
+++ 0201E.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+const int maxn=2e6+10;
+char s[maxn];
+bool bo[205];
+int n,m,tot,ch[205][3];
+void insert(char *s)
+{
+    int u=0,len=strlen(s);
+    for(int i=0;i<len;i++)
+    {
+        int c=s[i]-'a';
+        if(!ch[u][c])ch[u][c]=++tot;
+        u=ch[u][c];
+    }
+    bo[u]=true;
+}
+void dfs()
+{
+    
+}
+int main()
+{
+    scanf("%d%d",&n,&m);
+    while(n--)
+    {
+        scanf("%s",s);
+        insert(s);
+    }
+    while(m--)
+    {
+        scanf("%s",s);
+        
+        printf("%d\n",ans);
+    }
+    return 0;
+}
