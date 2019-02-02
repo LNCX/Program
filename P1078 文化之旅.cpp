a90810aaeef707a67bc1fc03ca71479c585a65a1
LNCX <1367595063@qq.com>
2019-02-02 22:07:42 +0800

    0202

--- P1078 文化之旅.cpp
+++ P1078 文化之旅.cpp
@@ -1,20 +1,36 @@
 #include<bits/stdc++.h>
 using namespace std;
-int read()
-{
-    int x=0;
-    char c=getchar();
-    while('0'>c||c>'9') c=getchar();
-    while('0'<=c&&c<='9')
-    {
-        x=(x<<3)+(x<<1)+c-'0';
-        c=getchar();
-    }
-    return x;
-}
+const int inf=0x3f3f3f3f;
+int c[500];
+int pd[500][500],a[500][500];
 int main()
 {
-    int n=read(),k=read(),m=read(),s=read(),t=read();
-    
+    int n,k,m,s,t;
+    scanf("%d%d%d%d%d",&n,&k,&m,&s,&t);
+    for(int i=1;i<=n;i++)
+        for(int j=1;j<=n;j++)
+            a[i][j]=inf;
+    for(int i=1;i<=n;i++)
+        scanf("%d",&c[i]);
+    for(int i=1;i<=k;i++)
+        for(int j=1;j<=k;j++)
+            scanf("%d",&pd[i][j]);
+    for(int i=1;i<=m;i++)
+    {
+        int x,y,z;
+        scanf("%d%d%d",&x,&y,&z);
+        if(z<a[x][y])
+        {
+            if(pd[c[x]][c[y]]==0) a[y][x]=z;
+            if(pd[c[y]][c[x]]==0) a[x][y]=z;  
+        }
+    }
+    for(int k=1;k<=n;k++)
+        for(int i=1;i<=n;i++)
+            for(int j=1;j<=n;j++)
+                if(a[i][k]+a[k][j]<a[i][j])
+                    a[i][j]=a[i][k]+a[k][j];
+    if(c[s]==c[t]||a[s][t]==inf) puts("-1");
+    else cout<<a[s][t]<<endl;
     return 0;
 }
