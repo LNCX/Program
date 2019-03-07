ec6be451653ee805136231804fc0ce3742fa180a
LNCX <1367595063@qq.com>
2019-03-07 12:15:55 +0800

    0202

--- 括号配对.cpp
+++ 括号配对.cpp
@@ -1,36 +1,25 @@
-#include<iostream>
-#include<string.h>
+#include<bits/stdc++.h>
 using namespace std;
-int judge(int l,int r,int &flag,char n[100]);
 int main()
 {
-	int flag=1;
-	char n[100];
-	cin>>n;
-	judge(0,strlen(n)-1,flag,n);
-	if(flag) cout<<"Yes"<<endl;
-	else cout<<"No"<<endl;
+    int n;
+    cin>>n;
+    while(n--)
+    {
+        string p;
+        stack<char>s;
+        cin>>p;
+        bool flag=1;
+        if(p.length()%2) flag=0;
+        for(int i=0;i<p.length()&&flag;i++)
+        {
+            if(p[i]=='('||p[i]=='[')
+                s.push(p[i]);
+            else if(s.size() && (p[i]==')'&&s.top()=='(') || (p[i]==']'&&s.top()=='[') )
+                s.pop();
+            else flag=0;
+        }
+        (flag&&s.empty())?puts("YES"):puts("NO");
+    }
+    return 0;
 }
-int judge(int l,int r,int &flag,char n[100])
-{
-	int a=0,b=0;
-	int loc[2]={0};
-	for(int i=l;i<=r;i++)
-	{
-		if(n[i]=='[') a++;
-		else if(n[i]==']') a--;
-		if(a==0) loc[0]=i;
-	}
-	for(int i=l;i<=r;i++)
-	{
-		if(n[i]=='(') b++;
-		else if(n[i]==')') b--;
-		if(b==0) loc[1]=i;
-	}
-	if((loc[0]-l)%2==1&&(loc[1]-l)%2==1)
-	{
-		judge(l,loc[0],flag,n);
-		judge(loc[0]+1,r,flag,n);	
-	}
-	else flag=0; 
-} 
