5e3fdaeb9c4f6ba26eb9e0051e4502ac7b373864
XY-cpp <1367595063@qq.com>
2019-03-13 17:08:12 +0800

    gu

---
+++ P1393 动态逆序对.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+const int maxn=2505;
+int a[maxn];
+int main()
+{
+    int n,m;
+    scanf("%d%d",&n,&m);
+    for(int i=1;i<=n;i++)
+        scanf("%d",&a[i]);
+    while(m--)
+    {
+        
+    }
+    return 0;
+}
