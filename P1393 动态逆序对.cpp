511dfda944d2aed7670d69bb9a8ee2d5021480ea
LNCX <1367595063@qq.com>
2019-03-15 15:08:56 +0800

    gugu

--- P1393 动态逆序对.cpp
+++ P1393 动态逆序对.cpp
@@ -1,16 +1,43 @@
 #include<bits/stdc++.h>
 using namespace std;
-const int maxn=2505;
-int a[maxn];
+const int maxn=4e4+5;
+struct point 
+{
+    int x,y,z;
+    bool operator<(const point &a) const
+    {
+        if(x!=a.x) return x<a.x;
+        if(y!=a.y) return y<a.y;
+        return z<a.z;
+    } 
+}a[maxn],t[maxn];
+int read()
+{
+    int x=0,f=1;
+    char c=getchar();
+    while(!isdigit(c)) {if(c=='-') f=-1;c=getchar();}
+    while(isdigit(c)) {x=(x<<3+x<<1+c-'0');c=getchar();}
+    return f==1?x:-x;
+}
+void CDQ(int l,int r)
+{
+    if(l==r) return ;
+    int mid=(l+r)>>1;
+    CDQ(l,mid),CDQ(mid+1,r);
+    
+}
 int main()
 {
     int n,m;
     scanf("%d%d",&n,&m);
     for(int i=1;i<=n;i++)
-        scanf("%d",&a[i]);
-    while(m--)
     {
-        
+        a[i].x=i;
+        a[i].y=read();
     }
+    for(int i=1;i<=m;i++)
+        a[read()].z=i;
+    sort(a+1,a+1+n);
+    CDQ(1,n);
     return 0;
 }
