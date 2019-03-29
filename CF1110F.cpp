9ed55dbe38a5a6791d7c96f8af7ef37718bf33dc
LNCX <1367595063@qq.com>
2019-03-29 15:51:29 +0800

    gu

--- CF1110F.cpp
+++ CF1110F.cpp
@@ -2,20 +2,20 @@
 using namespace std;
 typedef long long ll;
 const int maxn=5e5+5;
-const ll oo=0x7ffffffffffffff;
+const ll oo=1e17;
 int n,m;
 struct edge
 {
     int nxt,to,w;
 }e[maxn<<1];
-ll minx[maxn],dis[maxn],num[maxn],tag[maxn];
+ll minx[maxn<<2],dis[maxn],num[maxn],tag[maxn<<2];
 int head[maxn],tot,son[maxn],size[maxn];
 void add(int u,int v,int w)
 {
     e[++tot]=(edge){head[u],v,w};
     head[u]=tot;
 }
-void dfs1(int u,int fa)
+void dfs(int u,int fa)
 {
     size[u]=1;
     for(int i=head[u];i!=0;i=e[i].nxt)
@@ -23,7 +23,7 @@ void dfs1(int u,int fa)
         int v=e[i].to;
         if(v==fa) continue;
         dis[v]=dis[u]+e[i].w;
-        dfs1(v,u);
+        dfs(v,u);
         son[u]=max(son[u],son[v]);
         size[u]+=size[v];
     }
@@ -34,7 +34,7 @@ void build(int k,int l,int r)
 {
     if(l==r)
     {
-        minx[k]=num[k];
+        minx[k]=num[l];
         return ;
     }
     int mid=(l+r)>>1;
@@ -46,6 +46,7 @@ void pushdown(int k)
 {
     if(!tag[k]) return ;
     minx[k<<1]+=tag[k],minx[k<<1|1]+=tag[k];
+    tag[k<<1]+=tag[k],tag[k<<1|1]+=tag[k];
     tag[k]=0;
 }
 void add(int k,int l,int r,int x,int y,ll w)
@@ -74,9 +75,9 @@ ll query(int k,int l,int r,int x,int y)
 }
 void print()
 {
-    for(int i=1;i<=2*n;i++)
+    for(int i=1;i<=9;i++)
     {
-        minx[i]!=oo?printf("%lld ",minx[i]):printf("inf ");
+        cout<<son[i]<<" ";
     }
     puts("");
 }
@@ -89,23 +90,23 @@ int main()
         scanf("%d%d",&v,&w);
         add(i,v,w),add(v,i,w);        
     }
-    dfs1(1,0);
+    for(int i=1;i<=(n<<2);i++)
+        minx[i]=oo;
+    dfs(1,0);
     build(1,1,n);
-            print();
+    //print();
     while(m--)
     {
         int u,l,r;
         scanf("%d%d%d",&u,&l,&r);
-        if(size[u]==1) puts("0");
-        else 
-        {
-            add(1,1,n,1,n,dis[u]);
-            add(1,1,n,son[u]-size[u]+2,son[u],-(dis[u]<<1));
-            printf("%lld\n",query(1,1,n,l,r));
-            add(1,1,n,son[u]-size[u]+2,son[u],(dis[u])<<1);
-            add(1,1,n,1,n,-dis[u]);
-            //print();
-        }
+        add(1,1,n,1,n,dis[u]);
+        //print();
+        add(1,1,n,u,son[u],-(dis[u]<<1));
+        //print();
+        printf("%lld\n",query(1,1,n,l,r));
+        //print();
+        add(1,1,n,u,son[u],(dis[u])<<1);
+        add(1,1,n,1,n,-dis[u]);
     }
     return 0;
 }
