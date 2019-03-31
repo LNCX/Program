7aaa295bd2d70e0c6ab7e77c9b5c191ddfe164fb
LNCX <1367595063@qq.com>
2019-03-31 21:30:07 +0800

    gu

--- P3384 【模板】树链剖分.cpp
+++ P3384 【模板】树链剖分.cpp
@@ -17,8 +17,11 @@ struct edge
 {
     int nxt,to;
 }e[maxn<<1];
-int n,m,rt,mod,w[maxn],head[maxn],tot;
-int size[maxn],f[maxn],dep[maxn];
+int n,m,rt,mod,w[maxn];
+int head[maxn],tot;
+int sum[maxn<<2],tag[maxn<<1];
+int seg[maxn],rev[maxn],top[maxn];
+int size[maxn],f[maxn],dep[maxn],son[maxn];
 void edge_add(int u,int v)
 {
     e[++tot]=(edge){head[u],v};head[u]=tot;
@@ -35,6 +38,27 @@ void dfs1(int u,int fa)
         if(v==fa) continue;
         dfs1(v,u);
         size[u]+=size[v];
+        if(size[son[u]]<size[v]) son[u]=v;
+    }
+}
+void dfs2(int u,int fa)
+{
+    if(son[u])
+    {
+        seg[son[u]]=++seg[0];
+        top[son[u]]=top[u];
+        rev[seg[0]]=son[u];
+        dfs2(son[u],u);
+    }
+    for(int i=head[u];i!=0;i=e[i].nxt)
+    {
+        int v=e[i].to;
+        if(!top[v])
+        {
+            seg[v]=++seg[0];
+            top[v]=v;
+            rev[seg[0]]=v;
+        }
     }
 }
 
@@ -46,6 +70,16 @@ int main()
     for(int i=1;i<n;i++)
         edge_add(read(),read());
     dfs1(rt,0);
-    
+    seg[0]=seg[rt]=1;
+    rev[1]=top[rt]=rt;
+    dfs2();
+    for(int i=1;i<=m;i++)
+    {
+        int op=read(),x,y,z;
+        if(op==1)
+        {
+            x=read(),y=read(),z=read();
+        }
+    }
     return 0;
 }
