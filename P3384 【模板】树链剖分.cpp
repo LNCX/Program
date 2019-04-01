98385a759058afff2cf2ff81c1ef29c03e712a39
LNCX <1367595063@qq.com>
2019-04-01 13:24:00 +0800

    gu

--- P3384 【模板】树链剖分.cpp
+++ P3384 【模板】树链剖分.cpp
@@ -17,8 +17,8 @@ struct edge
 {
     int nxt,to;
 }e[maxn<<1];
-int n,m,rt,mod,w[maxn];
 int head[maxn],tot;
+int n,m,rt,p,num[maxn];
 int sum[maxn<<2],tag[maxn<<1];
 int seg[maxn],rev[maxn],top[maxn];
 int size[maxn],f[maxn],dep[maxn],son[maxn];
@@ -29,56 +29,138 @@ void edge_add(int u,int v)
 }
 void dfs1(int u,int fa)
 {
-    f[u]=fa;
-    size[u]=1;
-    dep[u]=dep[fa]+1;
-    for(int i=head[u];i!=0;i=e[i].nxt)
-    {
-        int v=e[i].to;
-        if(v==fa) continue;
-        dfs1(v,u);
-        size[u]+=size[v];
-        if(size[son[u]]<size[v]) son[u]=v;
-    }
+	f[u]=fa;
+	size[u]=1;
+	dep[u]=dep[fa]+1;
+	for(int i=head[u];i!=0;i=e[i].nxt)
+	{
+		int v=e[i].to;
+		if(v!=fa)
+		{
+			dfs1(v,u);
+			size[u]+=size[v];
+			if(size[v]>size[son[u]]) son[u]=v;
+		}
+	}
 }
-void dfs2(int u,int fa)
+void dfs2(int u,int f)
 {
-    if(son[u])
-    {
-        seg[son[u]]=++seg[0];
-        top[son[u]]=top[u];
-        rev[seg[0]]=son[u];
-        dfs2(son[u],u);
-    }
-    for(int i=head[u];i!=0;i=e[i].nxt)
-    {
-        int v=e[i].to;
-        if(!top[v])
-        {
-            seg[v]=++seg[0];
-            top[v]=v;
-            rev[seg[0]]=v;
-        }
-    }
+	if(son[u])
+	{
+		seg[son[u]]=++seg[0];
+		top[son[u]]=top[u];
+		rev[seg[0]]=son[u];
+		dfs2(son[u],u);
+	}
+	for(int i=head[u];i!=0;i=e[i].nxt)
+	{
+		int v=e[i].to;
+		if(!top[v])
+		{
+			seg[v]=++seg[0];
+			top[v]=v;
+			rev[seg[0]]=v;
+			dfs2(v,u);
+		}
+	}
 }
-
+void build(int k,int l,int r)
+{
+    if(l==r)
+    {
+        sum[k]=num[l]%p;
+        return ;
+    }
+    int mid=(l+r)>>1;
+    build(k<<1,l,mid);
+    build(k<<1|1,mid+1,r);
+    sum[k]=(sum[k<<1]+sum[k<<1|1])%p;
+}
+void pushdown(int k,int l,int r)
+{
+    if(tag[k]==0) return ;
+    int w=tag[k],mid=(l+r)>>1;
+    (tag[k<<1]+=w)%=p,(tag[k<<1|1]+=w)%=p;
+    (sum[k<<1]+=(mid-l+1)*w)%=p,(sum[k<<1|1]+=(r-mid)*w)%=p;
+    tag[k]=0;
+}
+void add(int k,int l,int r,int x,int y,int w)
+{
+    if(x<=l&&r<=y)
+    {
+        (tag[k]+=w)%=p;
+        (seg[k]+=w)%=p;
+        return ;
+    }
+    pushdown(k,l,r);
+    int mid=(l+r)>>1;
+    if(x<=mid) add(k<<1,l,mid,x,y,w);
+    if(y> mid) add(k<<1|1,mid+1,r,x,y,w);
+    sum[k]=(sum[k<<1]+sum[k<<1|1])%p;
+}
+int query(int k,int l,int r,int x,int y)
+{
+    if(x<=l&&r<=y) return sum[k];
+    pushdown(k,l,r);
+    int mid=(l+r)>>1,res=0;
+    if(x<=mid) (res+=query(k<<1,l,mid,x,y))%=p;
+    if(y< mid) (res+=query(k<<1|1,mid+1,r,x,y))%=p;
+    return res;
+}
+void LCA_add(int z,int x,int y)
+{
+    int fx=top[x],fy=top[y];
+	while(fx!=fy)
+	{
+		if(dep[fx]<dep[fy]) swap(x,y),swap(fx,fy);
+		add(1,1,seg[0],seg[fx],seg[x],z);
+		x=f[fx];fx=top[x];
+	}
+	if(dep[x]>dep[y]) swap(x,y);
+	add(1,1,seg[0],seg[x],seg[y],z);
+}
+int LCA_query(int x,int y)
+{
+    int fx=top[x],fy=top[y];
+	while(fx!=fy)
+	{
+		if(dep[fx]<dep[fy]) swap(x,y),swap(fx,fy);
+		query(1,1,seg[0],seg[fx],seg[x]);
+		x=f[fx];fx=top[x];
+	}
+	if(dep[x]>dep[y]) swap(x,y);
+	query(1,1,seg[0],seg[x],seg[y]);
+}
 int main()
 {
-    n=read(),m=read(),rt=read(),mod=read();
+    n=read(),m=read(),rt=read(),p=read();
     for(int i=1;i<=n;i++)
-        scanf("%d",&w[i]);
+        scanf("%d",&num[i]);
     for(int i=1;i<n;i++)
         edge_add(read(),read());
     dfs1(rt,0);
     seg[0]=seg[rt]=1;
     rev[1]=top[rt]=rt;
-    dfs2();
+    dfs2(rt,0);
+    build(1,1,seg[0]);
+    for(int i=1;i<=n;i++)   
+        printf("%d\n",top[i]);
     for(int i=1;i<=m;i++)
     {
-        int op=read(),x,y,z;
-        if(op==1)
+        int op=read();
+        if(op==1) LCA_add(read(),read(),read());
+        else if(op==2) printf("%d\n",LCA_query(read(),read()));
+        else if(op==3)
         {
-            x=read(),y=read(),z=read();
+            int x=read(),z=read();
+            //cerr<<seg[0]<<" "<<seg[x]<<" "<<seg[x]+size[x]-1<<endl;
+            add(1,1,seg[0],seg[x],seg[x]+size[x]-1,z);
+            
+        }
+        else if(op==4)
+        {
+            int x=read();
+            printf("%d\n",query(1,1,seg[0],seg[x],seg[x]+size[x]-1));
         }
     }
     return 0;
