e8d6cb12d4687e9232d5bab17539187e65b6d4f9
XY_cpp <1367595063@qq.com>
2019-04-26 17:36:52 +0800

    gu

--- P1402 酒店之王.cpp
+++ P1402 酒店之王.cpp
@@ -1,25 +1,22 @@
 #include<bits/stdc++.h>
 using namespace std;
-const int maxn=105,inf=0x3f3f3f3f;
-int n,m,tot=1,head[maxn],dep[maxn],vis[maxn],cur[maxn];
-struct Edge
+const int maxn=1005,inf=0x3f3f3f3f;
+int n,p,q,tot=1,head[maxn],dep[maxn],vis[maxn],cur[maxn];
+struct edge
 {
-    int to,cap,nxt,flow,from; 
-}e[maxn<<1];
-void Add(int from,int to,int cap)
+    int nxt,to,cap,flow; 
+}e[maxn*maxn<<1];
+void Add(int u,int v,int cap)
 {
-    e[++tot].nxt=head[from];
-    e[tot].cap=cap;
-    e[tot].from=from;
-    e[tot].to=to;
-    head[from]=tot;
+    e[++tot]=(edge){head[u],v,cap,0};
+    head[u]=tot;
 }
-bool bfs()
+bool bfs(int s,int t)
 {
-    for(int i=0;i<=n;i++)
+    for(int i=0;i<=t;i++)
         cur[i]=head[i],dep[i]=inf,vis[i]=0;
-    dep[1]=0;
-    queue<int>q;q.push(1);
+    dep[s]=0;
+    queue<int>q;q.push(s);
     while(!q.empty())
     {
         int u=q.front();
@@ -35,21 +32,20 @@ bool bfs()
             }
         }
     }
-    return vis[n];
+    return vis[t];
 }
-int dfs(int u,int add)
+int dfs(int u,int t,int add)
 {
-    if(u==n||add==0) return add;
+    if(u==t||add==0) return add;
     int flow=0;     
     for(int &i=cur[u];i!=0;i=e[i].nxt)
     {
         int v=e[i].to;
         if(e[i].cap-e[i].flow<=0||dep[u]+1!=dep[v]) continue;
-        int f=dfs(v,min(add,e[i].cap-e[i].flow));
+        int f=dfs(v,t,min(add,e[i].cap-e[i].flow));
         e[i].flow+=f;
         e[i^1].flow-=f;
-        flow+=f;
-        add-=f;
+        flow+=f,add-=f;
         if(add==0) break;
     }
     return flow;
@@ -57,31 +53,35 @@ int dfs(int u,int add)
 int dinic(int s,int t)
 {
     int flow=0;
-    while(bfs())
-        flow+=dfs(s,inf);
+    while(bfs(s,t))
+        flow+=dfs(s,t,inf);
     return flow;
 }
 int main()
 {
-    int n,p,q;
     scanf("%d%d%d",&n,&p,&q);
+    int s=2*n+q+p+1,t=s+1;
     for(int i=1;i<=n;i++)
         for(int j=1;j<=p;j++)
         {
             int x;
             scanf("%d",&x);
-            if(x==0) continue;
-            Add(j,p+i,1),Add(p+i,j,0);
+            if(x^1) continue;
+            Add(j,p+i,1);
+            Add(p+i,j,0);
         }
-    for(int i=p+1;i<=p+n;i++)
-        Add(i,i+n,1),Add(i+n,i,0);
     for(int i=1;i<=n;i++)
         for(int j=1;j<=q;j++)
         {
             int x;
             scanf("%d",&x);
-            if(x==0) continue;
-            Add(p+n+i,p+,)
+            if(x^1) continue;
+            Add(p+n+q+i,p+n+j,1);
+            Add(p+n+j,p+n+q+i,0);
         }
+    for(int i=1;i<=n;i++) Add(p+i,p+n+q+i,1),Add(p+n+q+i,p+i,0);
+    for(int i=1;i<=p;i++) Add(s,i,1),Add(i,s,0);
+    for(int i=p+1+n;i<=p+n+q;i++) Add(i,t,1),Add(t,i,0);
+    cout<<dinic(s,t)<<endl;
     return 0;
 }
