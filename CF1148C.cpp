3cbba3ba3e7aff294184722f84f5ad80aa6dff6e
XY_cpp <1367595063@qq.com>
2019-06-06 22:33:05 +0800

    gu

--- CF1148C.cpp
+++ CF1148C.cpp
@@ -1,55 +1,38 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-const int maxn=3e5+5;
-int n,a[maxn],b[maxn];
+const int maxn=333000;
+int n,d[maxn],p[maxn];
 vector< pair<int,int> >v;
 void change(int x,int y)
 {
-    swap(a[x],a[y]);
-    b[a[x]]=x,b[a[y]]=y;
-    v.push_back(make_pair(x,y));
+	if(x==y)return;
+	v.push_back(make_pair(x,y));
+	p[d[x]]=y,p[d[y]]=x;
+	swap(d[x],d[y]);
 }
 int main()
 {
     cin>>n;
-    for(int i=1;i<=n;i++)
-        scanf("%d",&a[i]),b[a[i]]=i;
-    for(int i=1;i<=n;i++)
+	for(int i=1;i<=n;i++)
+		cin>>d[i],p[d[i]]=i;
+	for(int i=1;i<=n;i++)
     {
-        if(a[i]<=n/2&&i!=a[i])
+		int e=n/2+i/2*(i%2?-1:1);
+		int j=p[e];
+		if(j<=n/2)
         {
-            if(i<=n/2)
-            {
-                change(i,n);
-                change(a[n],n);
-            }
-            else
-            {
-                change(1,i);
-                change(1,a[1]);
-                change(1,i);
-            }
-        }
-    }
-    for(int i=n/2+1;i<=n;i++)
-    {
-        int k=b[i];
-        if(k==a[k]) continue;
-        change(1,k);
-        change(1,a[1]);
-        change(1,k);
-    }
-    printf("%d\n",v.size());
-    for(auto i:v)
-        printf("%d %d\n",i.first,i.second);
-    // for(int i=1;i<n;i++)
-    // {
-    //     if(a[i]+1!=a[i+1])
-    //     {
-    //         puts("0");
-    //         return 0;
-    //     }
-    // }
-    // puts("xy");
-    return 0;
+			change(j,n);
+			if(e>n/2)change(n,1),change(1,e);
+			else change(n,e);
+		}
+		else
+        {
+			change(j,1);
+			if(e-1<n/2)change(n,1),change(n,e);
+			else change(1,e);
+		}
+	}
+	printf("%lu\n",v.size());
+	for(auto i:v)
+		printf("%d %d\n",i.first,i.second);
 }
