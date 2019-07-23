f401253a2f348c305abf50baa3fbf10b9937dd71
XY_cpp <1367595063@qq.com>
2019-07-23 18:06:58 +0800

    gu

--- P3690 [模板]Link Cut Tree(动态树)(模板题).cpp
+++ P3690 [模板]Link Cut Tree(动态树)(模板题).cpp
@@ -1,64 +1,67 @@
 #include<bits/stdc++.h>
 using namespace std;
-const int maxn=3e5+2;
-int f[maxn],v[maxn],s[maxn],r[maxn],hep[maxn],ch[maxn][2];
-inline int get(int x){return ch[f[x]][0]==x||ch[f[x]][1]==x;}
-inline void pushup(int x){s[x]=s[ch[x][1]]^s[ch[x][0]]^v[x];}
-inline void filp(int x){swap(ch[x][0],ch[x][1]);r[x]^=1;}
-inline void pushdown(int x)
+const int N=2e5+5;
+int a[N];
+struct LCT
 {
-    if(!r[x])return;r[x]=0;
-    if(ch[x][0])filp(ch[x][0]);
-    if(ch[x][1])filp(ch[x][1]);
-}
-inline void rotate(int x)
-{
-    int y=f[x],z=f[y],k=ch[y][1]==x,v=ch[x][!k];
-    if(get(y))ch[z][ch[z][1]==y]=x;ch[x][!k]=y,ch[y][k]=v;
-    if(v)f[v]=y;f[y]=x,f[x]=z;pushup(y),pushup(x);
-}
-inline void Splay(int x)
-{
-    int y=x,top=0;hep[++top]=y;
-    while(get(y))hep[++top]=y=f[y];
-    while(top)pushdown(hep[top--]);
-    while(get(x))
-    {
-        y=f[x],top=f[y];
-        if(get(y))
-           rotate((ch[y][0]==x)^(ch[top][0]==y)?x:y);
-        rotate(x);
-    }pushup(x);return;
-}
-inline void Access(int x)
-{
-    for(int y=0;x;x=f[y=x])
-       Splay(x),ch[x][1]=y,pushup(x);
-}
-inline void makeroot(int x){Access(x);Splay(x);filp(x);}
-inline int findroot(int x)
-{
-    Access(x);Splay(x);
-    while(ch[x][0])pushdown(x),x=ch[x][0];
-    return x;
-}
-inline void split(int x,int y){makeroot(x);Access(y);Splay(y);}
-inline void link(int x,int y){makeroot(x);if(findroot(y)!=x)f[x]=y;}
-inline void cut(int x,int y){split(x,y);if(findroot(y)==x&&f[x]==y&&!ch[x][1]) f[x]=ch[y][0]=0,pushup(y);}
+    private:
+        int s[N],f[N],v[N],ch[N][2],r[N],rt;
+        bool chk(int x){return ch[f[x]][1]==x;}
+        bool get(int x){return ch[f[x]][0]==x||ch[f[x]][1]==x;}
+        void pushup(int x){s[x]=s[ch[x][0]]^s[ch[x][1]]^a[x];}
+        void pushdown(int x)
+        {
+            if(!r[x]) return;
+            swap(ch[x][1],ch[x][0]);
+            r[ch[x][1]]^=1,r[ch[x][0]]^=1;
+            r[x]=0;
+        }
+        void pushadd(int x)
+        {
+            while(get(x)) pushadd(f[x]);
+            pushdown(x);
+        }
+        void rotate(int x)
+        {
+            int y=f[x],z=f[y],k=chk(x);
+            ch[y][k]=ch[x][k^1],f[ch[x][k^1]]=y;
+            ch[k][chk(y)]=x,f[x]=z;
+            ch[x][k^1]=y,f[y]=x;
+        }
+        void splay(int x)
+        {
+            pushadd(x);
+            while(get(x))
+            {
+                int y=f[x],z=f[y];
+                if(get(y)) chk(x)==chk(y)?rotate(y):rotate(x);
+                rotate(x);
+            }
+        }
+        void access(int x)
+        {
+            for(int y=0;x;y=x,x=f[x])
+                splay(x),ch[x][1]=y,pushup(x);
+        }
+    public:
+        int findrt(int x)
+        {
+            access(x),splay(x);
+            while(ch[x][0]) x=ch[x][0];
+            return x;
+        }
+        void makert(int x)
+        {
+            access(x),splay(x);
+            r[x]^=1,pushdown(x);
+        }
+        void split(int x,int y)
+        {
+            makert(x)
+        }
+};   
 int main()
 {
-    int n,m;
-    scanf("%d%d",&n,&m);
-    for(int i=1;i<=n;i++)
-        scanf("%d",&v[i]);
-    for(int i=1;i<=m;i++)
-    {
-        int op,x,y;
-        scanf("%d%d%d",&op,&x,&y);
-        if(op==0)split(x,y),printf("%d\n",s[y]);
-        else if(op==1)link(x,y);
-        else if(op==2)cut(x,y);
-        else Splay(x),v[x]=y;
-    }
+    
     return 0;
 }
