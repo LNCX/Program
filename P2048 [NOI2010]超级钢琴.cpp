6de9fe5385b5a561a1bc47bf02ba06a882544aea
XY_cpp <1367595063@qq.com>
2019-08-01 17:55:50 +0800

    gu

--- P2048 [NOI2010]超级钢琴.cpp
+++ P2048 [NOI2010]超级钢琴.cpp
@@ -1,23 +1,23 @@
 #include<bits/stdc++.h>
 using namespace std;
 const int N=5e5+5;
-int n,k,L,R,a[N],sum[N];
+int n,k,L,R,a[N],sum[N],ans,cnt;
 class RMQ
 {
     private:
-        int f[N][19],log[N];
+        int f[N][19],log[N]={-1};
     public:
         void init()
         {
             for(int i=1;i<=n;i++)
             {
-                f[i][0]=sum[i];
+                f[i][0]=i;
                 log[i]=log[i>>1]+1;
             }
-            for(int j=1;j<20;j++)
+            for(int j=1;(1<<j)<=n;j++)
                 for(int i=1;i+(1<<j)-1<=n;i++)
                 {
-                    int x=f[i][j-1],y=f[i+(1<<j-1)][j-1];
+                    int x=f[i][j-1],y=f[i+(1<<(j-1))][j-1];
                     f[i][j]=sum[x]>sum[y]?x:y;
                 }
         }
@@ -34,7 +34,7 @@ struct node
     node(int o,int l,int r):o(o),l(l),r(r),t(st.find(l,r)){}
     bool operator<(const node&k)const
     {
-        return sum[r]-sum[l-1]>sum[k.r]-sum[k.l-1];
+        return sum[t]-sum[o-1]>sum[k.t]-sum[k.o-1];
     }
 };
 priority_queue<node>q;
@@ -48,7 +48,13 @@ int main()
         q.push(node(i,i+L-1,min(i+R-1,n)));
     while(!q.empty())
     {
-        
+        int o=q.top().o,l=q.top().l,r=q.top().r,t=q.top().t;
+        cerr<<l<<" "<<r<<endl;
+        q.pop(),ans+=sum[t]-sum[o-1],cnt++;
+        if(cnt==k) break;
+        if(t!=l) q.push(node(o,o+L-1,t-1));
+        if(t!=r) q.push(node(o,t+1,min(n,o+R-1)));
     }
+    cout<<ans<<endl;
     return 0;
 }
