779a664a90a2e6e2f2466f8bfcb70dddd23f520d
XY_cpp <1367595063@qq.com>
2019-08-02 12:40:59 +0800

    gu

--- P2633 Count on a tree.cpp
+++ P2633 Count on a tree.cpp
@@ -1,15 +1,15 @@
 #include<bits/stdc++.h>
 using namespace std;
-const int N=8e5+5;
+const int N=1e5+5;
+int n,m,q,rt,a[N],b[N],dep[N],ans;
+int t[N],ch[N<<5][2],f[N][20],sum[N<<5];
 vector<int>e[N];
-int f[N][20],ch[N<<5][2],t[N],sum[N<<5],tag;
-int s,a[N],b[N],vis[N],dep[N],size[N],cnt,rt[N];
 void update(int pre,int &k,int l,int r,int x)
-{ 
-    if(l>x||r<x) return;
-    k=++cnt;
+{
+    if(l>x||r<x) return ;
+    k=++rt;
     ch[k][0]=ch[pre][0],ch[k][1]=ch[pre][1],sum[k]=sum[pre]+1;
-    if(l==r) return ;
+    if(l==r) return;
     int mid=(l+r)>>1;
     update(ch[pre][0],ch[k][0],l,mid,x);
     update(ch[pre][1],ch[k][1],mid+1,r,x);
@@ -17,18 +17,15 @@ void update(int pre,int &k,int l,int r,int x)
 void dfs(int u,int fa)
 {
     f[u][0]=fa;
-    size[u]=vis[u]=1;
     dep[u]=dep[fa]+1;
-    rt[u]=(fa?rt[fa]:u);
     for(int i=1;i<=17;i++)
         f[u][i]=f[f[u][i-1]][i-1];
-    int x=lower_bound(b+1,b+1+s,a[u])-b;
-    update(t[fa],t[u],1,s,x);
+    int x=lower_bound(b+1,b+1+m,a[u])-b;
+    update(t[fa],t[u],1,m,x);
     for(auto v:e[u])
     {
         if(v==fa) continue;
         dfs(v,u);
-        size[u]+=size[v];
     }
 }
 int lca(int x,int y)
@@ -39,7 +36,7 @@ int lca(int x,int y)
         if(dep[f[x][i]]>=dep[y])
             x=f[x][i];
         if(x==y) return x;
-    }   
+    }
     for(int i=17;i>=0;i--)
         if(f[x][i]!=f[y][i])
             x=f[x][i],y=f[y][i];
@@ -53,48 +50,31 @@ int query(int u,int v,int x,int fx,int l,int r,int k)
     if(k<=w) return query(ch[u][0],ch[v][0],ch[x][0],ch[fx][0],l,mid,k);
     else return query(ch[u][1],ch[v][1],ch[x][1],ch[fx][1],mid+1,r,k-w);
 }
-void merge(int u,int v)
-{
-    if(size[rt[v]]>size[rt[u]]) swap(u,v);
-    e[u].push_back(v),e[v].push_back(u);
-    dfs(v,u);
-}
 int main()
 {
-    int n,m,q;
-    scanf("%d%d%d%d",&n,&n,&m,&q);
+    scanf("%d%d",&n,&q);
     for(int i=1;i<=n;i++)
     {
         scanf("%d",&a[i]);
         b[i]=a[i];
     }
-    for(int i=1;i<=m;i++)
+    for(int i=1;i<n;i++)
     {
         int u,v;
         scanf("%d%d",&u,&v);
-        e[u].push_back(v);
-        e[v].push_back(u);
+        e[u].push_back(v),e[v].push_back(u);
     }
     sort(b+1,b+1+n);
-    s=unique(b+1,b+1+n)-b-1;
-    for(int i=1;i<=n;i++)
-        if(!vis[i])
-            dfs(i,0);
-    int ans=0;
-    for(int i=1;i<=q;i++)
+    m=unique(b+1,b+1+n)-b-1;
+    dfs(1,0);
+    while(q--)
     {
-        char c[10];
         int u,v,k;
-        scanf("%s %d %d",c,&u,&v);
-        u^=ans,v^=ans;
-        if(*c=='Q')
-        {
-            scanf("%d",&k),k^=ans;
-            int x=lca(u,v);
-            ans=b[query(t[u],t[v],t[x],t[f[x][0]],1,s,k)];
-            printf("%d\n",ans);
-        }
-        else merge(u,v);
+        scanf("%d%d%d",&u,&v,&k);
+        u^=ans;
+        int x=lca(u,v);
+        ans=b[query(t[u],t[v],t[x],t[f[x][0]],1,m,k)];
+        printf("%d\n",ans);
     }
     return 0;
 }
