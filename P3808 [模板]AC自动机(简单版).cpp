49f2914aec2ab6a329a614f94e269d7baf886b37
XY_cpp <1367595063@qq.com>
2019-08-06 21:14:07 +0800

    gu

--- P3808 [模板]AC自动机(简单版).cpp
+++ P3808 [模板]AC自动机(简单版).cpp
@@ -1,49 +1,66 @@
 #include<bits/stdc++.h>
-const int maxn=5e5+5;
 using namespace std;
-queue<int>q;
-char s[maxn<<1];
-int c[maxn][26],val[maxn],fail[maxn],cnt;
-void ins(char *s)
+const int N=1e6+5;
+class AC_automaton
 {
-	int len=strlen(s);int now=0;
-	for(int i=0;i<len;i++)
-	{
-		int v=s[i]-'a';
-		if(!c[now][v])c[now][v]=++cnt;
-		now=c[now][v];
-	}
-	val[now]++;
-}
-void build()
-{
-	for(int i=0;i<26;i++)if(c[0][i])fail[c[0][i]]=0,q.push(c[0][i]);
-	while(!q.empty())
-	{
-		int u=q.front();q.pop();
-		for(int i=0;i<26;i++)
-		if(c[u][i])fail[c[u][i]]=c[fail[u]][i],q.push(c[u][i]);
-		else c[u][i]=c[fail[u]][i];
-	}
-}
-int query(char *s)
-{
-	int len=strlen(s);int now=0,ans=0;
-	for(int i=0;i<len;i++)
-	{
-		now=c[now][s[i]-'a'];
-		for(int t=now;t&&~val[t];t=fail[t])ans+=val[t],val[t]=-1;
-	}
-	return ans;
-}
+    private:
+        int ch[N][26],bo[N],tot=1,nxt[N];
+        void build()
+        {
+            queue<int>q;
+            for(int i=0;i<26;i++)
+                ch[0][i]=1;
+            q.push(1);
+            while(!q.empty())
+            {
+                int u=q.front();q.pop();
+                for(int c=0;c<26;c++)
+                {
+                    if(!ch[u][c]) ch[u][c]=ch[nxt[u]][c];
+                    else
+                    {
+                        q.push(ch[u][c]);
+                        nxt[ch[u][c]]=ch[nxt[u]][c];
+                    }
+                }
+            }
+        }
+    public:
+        void insert(char *s)
+        {
+            int n=strlen(s),u=1;
+            for(int i=0;i<n;i++)
+            {
+                int c=s[i]-'a';
+                if(!ch[u][c]) ch[u][c]=++tot;
+                u=ch[u][c];
+            }
+            bo[u]++;
+        }
+        int find(char *s)
+        {
+            build();
+            int res=0,u=1,n=strlen(s);
+            for(int i=0;i<n;i++)
+            {
+                u=ch[u][s[i]-'a'];
+                for(int t=u;t&&~bo[t];t=nxt[t])
+					res+=bo[t],bo[t]=-1;
+            }
+            return res;
+        }
+}t;
+char s[N];
 int main()
 {
-	int n;
+    int n;
     scanf("%d",&n);
     for(int i=1;i<=n;i++)
-		scanf("%s",s),ins(s);
-    build();
+    {
+        scanf("%s",s);
+        t.insert(s);
+    }
     scanf("%s",s);
-    printf("%d\n",query(s));
+    printf("%d\n",t.find(s));
     return 0;
 }
