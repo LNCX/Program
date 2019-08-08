97c744b0292b7c2a245ca9fd758f66758cf4869f
XY_cpp <1367595063@qq.com>
2019-08-08 13:27:53 +0800

    gu

--- P3261 [JLOI2015]城池攻占.cpp
+++ P3261 [JLOI2015]城池攻占.cpp
@@ -4,87 +4,74 @@ typedef long long ll;
 const int N=3e5+5;
 vector<int>e[N];
 int n,m,ans1[N],ans2[N];
-int rt[N],ch[N][2],dis[N],size[N];
-ll tag[N][3],val[N],h[N],a[N],v[N],s[N],c[N];
+int ch[N][2],rt[N],dis[N];
+ll mul[N],add[N],dep[N],a[N],h[N],v[N],c[N],val[N];
+void down(int x,ll y,ll z)
+{
+	if(!x) return;
+	val[x]*=y,val[x]+=z;
+	mul[x]*=y,add[x]*=y,add[x]+=z;
+}
 void pushdown(int x)
 {
-    if(!x) return;
-    for(int i=0;i<2;i++)
-        if(ch[x][i])
-        {
-            tag[ch[x][i]][0]+=tag[x][0];
-            tag[ch[x][i]][1]*=tag[x][1];
-            tag[ch[x][i]][2]+=tag[x][2];
-            val[ch[x][i]]*=tag[x][1];
-            val[ch[x][i]]+=tag[x][0];
-            ans2[ch[x][i]]+=tag[x][2];
-        }
-    tag[x][1]=1;
-    tag[x][0]=tag[x][2]=0;
-}
-void sign(int x,ll y,int t)
-{
-    if(t==0)
-    {
-        tag[x][t]+=y;
-        val[x]+=y;
-    }
-    if(t==1)
-    {
-        tag[x][t]*=y;
-        tag[x][t^1]*=y;
-        val[x]*=y;
-    }
-    tag[x][2]++;
-    ans2[x]++;
+	down(ch[x][0],mul[x],add[x]);
+	down(ch[x][1],mul[x],add[x]);
+	mul[x]=1,add[x]=0;
 }
 int merge(int x,int y)
 {
-    pushdown(x),pushdown(y);
-    if(!x||!y) return x|y;
-    if(val[x]>val[y]) swap(x,y);
-    ch[x][1]=merge(ch[x][1],y);
-    if(dis[ch[x][1]]>dis[ch[x][0]])
-        swap(ch[x][1],ch[x][0]);
-    dis[x]=dis[ch[x][1]]+1;
-    size[x]=size[ch[x][0]]+size[ch[x][1]]+1;
-    return x;
+	if(!x||!y) return x|y;
+	pushdown(x);pushdown(y);
+	if(val[x]>val[y]) swap(x,y);
+	ch[x][1]=merge(ch[x][1],y);
+	if(dis[ch[x][0]]<dis[ch[x][1]]) 
+        swap(ch[x][0],ch[x][1]);
+	dis[x]=dis[ch[x][1]]+1;
+	return x;
 }
 int pop(int x){return merge(ch[x][0],ch[x][1]);}
-void dfs(int u,int fa)
+void dfs(int u)
 {
-    for(auto v:e[u])
+	for(auto v:e[u])
     {
-        if(v==fa) continue;
-        dfs(v,u);
-        rt[u]=merge(rt[u],rt[v]);
-    }
-    while(val[rt[u]]<h[u])
+		dep[v]=dep[u]+1;
+		dfs(v);
+		rt[u]=merge(rt[u],rt[v]);
+	}
+	while(rt[u]&&val[rt[u]]<h[u])
     {
-        ans1[u]++;
-        rt[u]=pop(rt[u]);
-    }
-    sign(rt[u],v[u],a[u]);
+		pushdown(rt[u]);
+		ans1[u]++;ans2[rt[u]]=dep[c[rt[u]]]-dep[u];
+		rt[u]=pop(rt[u]);
+	}
+	if(a[u]) down(rt[u],v[u],0);
+	else down(rt[u],1,v[u]);
 }
 int main()
 {
     scanf("%d%d",&n,&m);
-    for(int i=1;i<=n;i++)
+	for(int i=1;i<=n;i++)
         scanf("%lld",&h[i]);
-    for(int i=2;i<=n;i++)
+	for(int i=2;i<=n;i++)
     {
-        ll f;
+        int f;
         scanf("%lld%lld%lld",&f,&a[i],&v[i]);
         e[f].push_back(i);
     }
-    for(int i=1;i<=m;i++)
+	for(int i=1;i<=m;i++)
     {
-        scanf("%lld%lld",&s[i],&c[i]);
-        val[i]=s[i],tag[i][1]=1;
-        rt[c[i]]=merge(i,c[i]);
-    }
-    dfs(1,0);
-    for(int i=1;i<=n;i++) printf("%d\n",ans1[i]);
-    for(int i=1;i<=m;i++) printf("%d\n",ans2[i]);
-    return 0;
+        scanf("%lld%lld",&val[i],&c[i]);
+		mul[i]=1;
+		rt[c[i]]=merge(rt[c[i]],i);
+	}
+    dep[1]=1,dfs(1);
+	while(rt[1])
+    {
+		pushdown(rt[1]);
+		ans2[rt[1]]=dep[c[rt[1]]];
+		rt[1]=pop(rt[1]);
+	}
+	for(int i=1;i<=n;i++) printf("%d\n",ans1[i]);
+	for(int i=1;i<=m;i++) printf("%d\n",ans2[i]);
+	return 0;
 }
