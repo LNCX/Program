e126ad02c8b64b7fb27f409c87c55629ac5660f3
XY_cpp <1367595063@qq.com>
2019-08-08 12:41:49 +0800

    hu

--- P3261 [JLOI2015]城池攻占.cpp
+++ P3261 [JLOI2015]城池攻占.cpp
@@ -4,19 +4,23 @@ typedef long long ll;
 const int N=3e5+5;
 vector<int>e[N];
 int n,m,ans1[N],ans2[N];
-int rt[N],ch[N][2],dis[N];
-ll h[N],a[N],v[N],s[N],c[N];
-ll tag[N][2],val[N];
+int rt[N],ch[N][2],dis[N],size[N];
+ll tag[N][3],val[N],h[N],a[N],v[N],s[N],c[N];
 void pushdown(int x)
 {
+    if(!x) return;
     for(int i=0;i<2;i++)
         if(ch[x][i])
         {
             tag[ch[x][i]][0]+=tag[x][0];
             tag[ch[x][i]][1]*=tag[x][1];
+            tag[ch[x][i]][2]+=tag[x][2];
             val[ch[x][i]]*=tag[x][1];
             val[ch[x][i]]+=tag[x][0];
-        }   
+            ans2[ch[x][i]]+=tag[x][2];
+        }
+    tag[x][1]=1;
+    tag[x][0]=tag[x][2]=0;
 }
 void sign(int x,ll y,int t)
 {
@@ -31,6 +35,8 @@ void sign(int x,ll y,int t)
         tag[x][t^1]*=y;
         val[x]*=y;
     }
+    tag[x][2]++;
+    ans2[x]++;
 }
 int merge(int x,int y)
 {
@@ -41,6 +47,7 @@ int merge(int x,int y)
     if(dis[ch[x][1]]>dis[ch[x][0]])
         swap(ch[x][1],ch[x][0]);
     dis[x]=dis[ch[x][1]]+1;
+    size[x]=size[ch[x][0]]+size[ch[x][1]]+1;
     return x;
 }
 int pop(int x){return merge(ch[x][0],ch[x][1]);}
@@ -52,9 +59,12 @@ void dfs(int u,int fa)
         dfs(v,u);
         rt[u]=merge(rt[u],rt[v]);
     }
-    while(val[pop(rt[u])])
-    sign(rt[u],)
-    
+    while(val[rt[u]]<h[u])
+    {
+        ans1[u]++;
+        rt[u]=pop(rt[u]);
+    }
+    sign(rt[u],v[u],a[u]);
 }
 int main()
 {
@@ -63,7 +73,7 @@ int main()
         scanf("%lld",&h[i]);
     for(int i=2;i<=n;i++)
     {
-        int f;
+        ll f;
         scanf("%lld%lld%lld",&f,&a[i],&v[i]);
         e[f].push_back(i);
     }
