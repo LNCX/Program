a43c74326e89de9524cfd6525c9a0afecf9d466a
XY_cpp <1367595063@qq.com>
2019-08-09 19:16:46 +0800

    gu

--- P2613 [模板]有理数取余.cpp
+++ P2613 [模板]有理数取余.cpp
@@ -1,33 +1,49 @@
 #include<bits/stdc++.h>
 using namespace std;
-typedef long long ll;
-const int mod=192608917;
-void read(){}
-template<typename T,typename... Ts>
-inline void read(T &arg,Ts&... args)
+const int mod=19260817,N=1e4+5;
+class lint
 {
-    T x=0,f=1;
-    char c=getchar();
-    while(!isdigit(c)){if(c=='-') f=-1;c=getchar();}
-    while(isdigit(c)){x=(x<<3)+(x<<1)+(c-'0');c=getchar();}
-    arg=x*f;
-    read(args...);
-}   
-ll power(ll a,ll b)
+    private:
+        int len,d[N];
+        friend lint operator*(const lint a,const lint b)
+        {
+            
+        }
+        friend lint operator%(const lint a,int b)
+        {
+
+        }
+    
+    public:
+        lint()
+        {
+            len=1;
+            memset(d,0,sizeof(d));
+        }      
+        void read()
+        {
+            
+        }
+        void print()
+        {
+
+        }
+};
+lint power(lint a,lint b)
 {
-    a%=mod;
-    ll res=1;
+    a=a%mod;
+    lint res=1;
     while(b)
     {
         if(b&1) (res*=a)%=mod;
-        b>>=1,(a=a*a)%mod;
+        b>>=1,(a*=a)%=mod;
     }
     return res;
 }
 int main()
 {
-    ll a,b;
-    read(a,b);
+    lint a,b;
+    a.read(),b.read();
     cout<<a*power(b,mod-2)%mod<<endl;
     return 0;
 }
--- [LnOI2019]来者不拒，去者不追.cpp
+++ [LnOI2019]来者不拒，去者不追.cpp
@@ -1,6 +1,10 @@
 #include<bits/stdc++.h>
 using namespace std;
+#define lowbit(x) (x&(-x))
 typedef long long ll;
+const int N=100;
+ll p[N],c[N],now,ans[N];
+int n,m,a[N],blg[N],block;
 void read(){}
 template<typename T,typename... Ts>
 inline void read(T &arg,Ts&... args)
@@ -12,27 +16,86 @@ inline void read(T &arg,Ts&... args)
     arg=x*f;
     read(args...);
 }
-const int N=1e5+5;
-ll a[N],b[N];
-int main()
+struct node
 {
-    int n,m;
+    int l,r,id;
+    friend bool operator<(const node & a,const node &b)
+    {
+        return (a.l/block)^(b.l/block)?a.l<b.l:(((a.l/block)&1)?a.r<b.r:a.r>b.r);
+    }
+}q[N];
+void update(int x,int y)
+{
+    int u=x;  
+    while(u<=n)
+    {
+        p[u]+=y,c[u]+=(ll)u*y;
+        u+=lowbit(u);
+    }
+}
+int num(int x)
+{
+    int res=0;
+    while(x>0)
+    {   
+        res+=p[x];
+        x-=lowbit(x);
+    }           
+    return res;
+}
+ll ask(int x)
+{
+    ll res=0;
+    while(x>0)
+    {   
+        res+=c[x];
+        x-=lowbit(x);
+    }           
+    return res;
+}
+void add(int x)
+{
+    x=a[x];
+    now+=(ll)(num(x-1)+1)*x;
+    now+=ask(n)-ask(x);
+    update(x,1);
+}
+void del(int x)
+{
+    x=a[x];
+    now-=(ll)(num(x-1)+1)*x;
+    now-=ask(n)-ask(x);
+    update(x,-1);
+}
+void moque()
+{
+    int l=1,r=0;
+    for(int i=1;i<=m;i++)
+    {
+        int ql=q[i].l,qr=q[i].r;
+        while(ql<l) add(--l);
+        while(ql>l) del(l++);
+        while(qr<r) del(r--); 
+        while(qr>r) add(++r);
+        ans[q[i].id]=now;
+    }
+}
+int main()
+{   
     read(n,m);
-    for(int i=1;i<=n;i++) read(a[i]);
-    while(m--)
+    for(int i=1;i<=n;i++)
+        read(a[i]);
+    for(int i=1;i<=m;i++)
     {
         int l,r;
-        scanf("%d%d",&l,&r);
-        for(int i=l;i<=r;i++) b[i]=a[i];
-        sort(b+l,b+r+1);
-        ll cnt=1,now=1,ans=b[l];
-        for(int i=l+1;i<=r;i++)
-        {
-            cnt++;
-            if(b[i]!=b[i-1]) now=cnt;
-            ans+=b[i]*now;
-        }     
-        printf("%lld\n",ans);
+        read(l,r);
+        cerr<<l<<" "<<r<<endl;
+        q[i]=(node){l,r,i};
     }
+    block=sqrt(n);
+    sort(q+1,q+1+n);
+    moque();
+    for(int i=1;i<=m;i++)
+        printf("%lld\n",ans[i]);
     return 0;
 }
