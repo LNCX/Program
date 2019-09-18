6c281c9693290ba14714f0d67d400bde50d81c77
XY_cpp <1367595063@qq.com>
2019-09-18 13:23:06 +0800

    gu

--- P3123 [USACO15OPEN]Bessie Goes Moo.cpp
+++ P3123 [USACO15OPEN]Bessie Goes Moo.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include<bits/stdc++.h>
 using namespace std;
 long long ans=0;
@@ -44,51 +43,4 @@ int main()
     dfs(0);
     cout<<ans;
     return 0;
-=======
-#include<bits/stdc++.h>
-using namespace std;
-long long ans=0;
-int n,g[10][10],x[10],v[233][8]={0};
-void dfs(int now)
-{
-    if(now==7)
-    {
-        long long sum=1ll*(x[0]+x[1]+x[2]+x[2]+x[3]+x[1])*(x[4]+x[5]+x[1]+x[2])*(x[6]+x[5]*2);
-        if(sum%7==0)
-        {
-            long long tmp=1;
-            for (int i=0;i<7;i++)
-                tmp*=g[i][x[i]];
-            ans+=tmp;
-        }
-        return ;
-    }
-    for (int i=0;i<7;i++)
-    {
-        if(!g[now][i]) continue;
-        x[now]=i;
-        dfs(now+1);
-    }
-}
-int main()
-{
-    scanf("%d",&n);
-    for(int i=1;i<=n;i++)
-    {
-        char c;
-        int l;
-        cin>>c>>l;
-        l=(l%7+7)%7;
-        if(c=='B') g[0][l]++;
-        if(c=='E') g[1][l]++;
-        if(c=='S') g[2][l]++;
-        if(c=='I') g[3][l]++;
-        if(c=='G') g[4][l]++;
-        if(c=='O') g[5][l]++;
-        if(c=='M') g[6][l]++;        
-    }
-    dfs(0);
-    cout<<ans;
-    return 0;
->>>>>>> 55f1b4987538945f67c9437cf19c2dafadb785ab
 }
