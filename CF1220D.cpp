2a74d1af687770c6f296ea322ce59d67fa7c4a7a
XY_cpp <1367595063@qq.com>
2019-09-21 07:57:51 +0800

    gu

--- CF1220D.cpp
+++ CF1220D.cpp
@@ -1,12 +1,25 @@
 #include<bits/stdc++.h>
+#define int long long
 using namespace std;
 const int N=2e5+5;
-int a[N],n;
-int main()
+int a[N],b[N],c[N],n,ans,k;
+signed main()
 {
-    scanf("%d",&n);
+    scanf("%lld",&n);
     for(int i=1;i<=n;i++)
-        scanf("%d",&a[i]);
-        
+    {
+        scanf("%lld",&b[i]);
+        int t=0,x=b[i];
+        while(x%2==0&&x) x>>=1ll,t++;
+        a[t]++,c[i]=t;
+    }
+    if(n==1) printf("1 %lld\n",b[1]);
+    for(int i=0;i<=20;i++)
+        if(ans<a[i]) ans=a[i],k=i;
+    printf("%d\n",n-ans);
+    for(int i=1;i<=n;i++)
+        if(c[i]!=k)
+            printf("%lld ",b[i]);
+    puts("");
     return 0;
 }
