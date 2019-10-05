bd28f93a46015ec1cb1be6f4dfb0532f33bd2c8c
XY_cpp <1367595063@qq.com>
2019-10-05 16:13:08 +0800

    gu

--- CF126B.cpp
+++ CF126B.cpp
@@ -2,13 +2,21 @@
 using namespace std;
 const int N=1e6+5;
 char s[N];
+int nxt[N],n,maxx;
 int main()
 {
     scanf("%s",s+1);
-    int n=strlen(s+1);
-    for(int i=1;i<=n;i++)
+    n=strlen(s+1);
+    for(int i=1,j=0;i<=n;i++)
     {
-        
+        while(s[i+1]!=s[j+1]&&j>0) j=nxt[j];
+        if(s[i+1]==s[j+1]) j++;
+        nxt[i+1]=j;
+        if(i+1!=n) maxx=max(maxx,nxt[i+1]);
     }
+    int x=nxt[n];
+    while(x>maxx) x=nxt[x];
+    if(!x)puts("Just a legend");
+    else for(int i=1;i<=x;i++) printf("%c",s[i]);
     return 0;
 }
